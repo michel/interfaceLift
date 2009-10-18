67389f91d266114f0cb1a59f1d680a6f1a08d492
Michel de Graaf <michel@re-invention.nl>
2009-10-18 21:10:34 +0200

    added new theme

--- Rakefile
+++ Rakefile
@@ -5,8 +5,8 @@ begin
   require 'jeweler'
   Jeweler::Tasks.new do |gem|
     gem.name = "interfacelift"
-    gem.summary = %Q{TODO: one-line summary of your gem}
-    gem.description = %Q{TODO: longer description of your gem}
+    gem.summary = %Q{Installs templates in rails applications}
+    gem.description = %Q{Collection of cool layouts to use in your rails apps.}
     gem.email = "michel@re-invention.nl"
     gem.homepage = "http://github.com/michel/interfacelift"
     gem.authors = ["Michel de Graaf"]
--- lib/interfacelift.rb
+++ lib/interfacelift.rb
@@ -13,6 +13,13 @@ module  InterfaceLift
       raise "Theme #{theme} not found" unless File.directory? theme_path   
     end
     
+    
+    def self.list
+       Dir.foreach("#{APP_ROOT}/lib/templates") do |t|
+         puts t unless t == "shared" || t == ".." || t == "."  ||  t == ".DS_Store"      
+       end
+    end
+    
     def install!  
       install_images
       install_stylesheets
@@ -22,23 +29,23 @@ module  InterfaceLift
     end
     
     def install_images
-      FileUtils.cp_r "#{@theme_path}/images","#{path}/public"    
+      FileUtils.cp_r "#{@theme_path}/images","#{path}/public"  if File.directory?  "#{@theme_path}/images"
     end
     
     def install_stylesheets
-      FileUtils.cp_r "#{@theme_path}/stylesheets","#{path}/public"    
+      FileUtils.cp_r "#{@theme_path}/stylesheets","#{path}/public"  if File.directory?  "#{@theme_path}/stylesheets"                 
     end
     
     def install_layouts
-      FileUtils.cp_r "#{@theme_path}/layouts","#{path}/app/views"    
+      FileUtils.cp_r "#{@theme_path}/layouts","#{path}/app/views"  if File.directory?  "#{@theme_path}/layouts"                 
     end
     
     def install_javascript
-      FileUtils.cp_r "#{@theme_path}/javascripts","#{path}/public"    
+      FileUtils.cp_r "#{@theme_path}/javascripts","#{path}/public"  if File.directory?  "#{@theme_path}/javascripts"                 
     end
     
     def install_shared
-      FileUtils.cp_r "#{SHARED}/icons","#{path}/public/images/"          
+      FileUtils.cp_r "#{SHARED}/icons","#{path}/public/images/"  if File.directory?  "#{@theme_path}/icons"                        
     end
     
   end
