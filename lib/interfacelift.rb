9497375eb422260aabaf5a57f5450eba855ea7c2
Jeroen van Schagen <jeroen@it-techniek.nl>
2009-11-18 00:05:22 +0100

    Added catalog and test specs

--- lib/interfacelift.rb
+++ lib/interfacelift.rb
@@ -1,6 +1,8 @@
 module  InterfaceLift  
    VERSION = "0.0.1"
+   DEFAULT_CATELOG_PATH =  "~/.interfacelift"
 end
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__))
+require "catalog"
 require "installer"
 require "theme_manager"
